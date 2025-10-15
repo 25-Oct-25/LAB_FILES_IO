eb81803902a0ebae7eddeb92b608359291bb60c9
Sana <sanaalboshi77@gmail.com>
2025-10-16 02:04:54 +0300

    sana

--- main.py
+++ main.py
@@ -11,22 +11,27 @@ while True:
         print("Thank you for using the To-Do List! Come back soon! 👋")
         break
 
+
     elif choice == "y":
+         # Add a new task
         item = input("Add new task 🖊️: ").strip()
         with open(file_name, "a", encoding="utf-8") as f:
             f.write(item + "\n")
         print(f"'{item}' added!\n")
 
     elif choice == "n":
+            # Show the list of tasks
+
         list_choice = input("Do you want to see your tasks 👀? (y/n): ").strip().lower()
         if list_choice == "y":
             try:
                 with open(file_name, "r", encoding="utf-8") as f:
                     for line in f:
                         print(line.strip())  # عرض المهام
+                        
             except FileNotFoundError:
                 print("Your list is empty.")
-        print()  # سطر فارغ للوضوح
+        print()  
 
     else:
         print("Invalid input❗Please type 'y', 'n', or 'exit'.\n")
