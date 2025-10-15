d60c987a97f22805ba89580669b2d9fd2431e92b
Hissah Saad <hissahsaad23@gmail.com>
2025-10-15 23:06:37 +0300

    bounce Q update - Hissah alkharboush

--- bonus.py
+++ bonus.py
@@ -1,46 +1,41 @@
 import json
-from datetime import date
+import datetime
 import time
 
-class To_Do:
+#define dictionary variables
+task_title =""
+task_date = datetime.date(2025,10,15)
+task_time = datetime.time()
 
-    #Create class attributes
-    def __init__(self, title:str,task_date:date, task_time:time, done:bool):
-        self.title = title
-        self.task_date = task_date
-        self.task_time = task_time
-        self.done = False
-    
-        #Create add task method
-    """    def add_task (self, task_title:str, task_date:date, task_time:time, done:bool ):
-            file"""
-    pass
-    
-project1 = To_Do("",date.today,time.time,False)
-done = project1.done
+#Create a to-do list has type dictionary
+to_do_list = {
+    'title': task_title,
+    'date': task_date,
+    'time' : task_time,
+    'done' : False
+}
 
-i = 1
+i = 0
+
 while True:
-    user_input = input("Do you want add a new item? type 'y' for yes, and 'n' for no")
-    if user_input == 'y':
-        project1.title = input("Enter your task title ")
-        project1.task_date = input("Enter your task date (year , month, day) ")
-        project1.task_time = input("Enter your task time (hour,mintus,seconds)")
+    i += 1
+    #add value to variable and write to file
+    print('As following instruction enter your To-Do task list:')
+    task_title = input('Write now your task title: ')
+    year = int(input('Write now your task year : '))
+    month = int(input('Write now your task month : '))
+    day = int(input('Write now your task day : '))
+    task_date = datetime.date(year,month,day)
 
-        #Create a file to write task on it
-        file = open('to_do.txt', 'a+', encoding="UTF-8")
-        file.write('\n',i ,'- ',project1.title, ' - ',project1.task_date, " ", project1.task_time,' - ' )
-        if done == False:
-            print("NOT DONE")
-        else: print ("DONE")
-        file.close()
-    elif user_input == 'n':
-        user_input= input("Do you want to list your To-Do items ? type 'y' for yes and 'n' for no")
-        if user_input == 'y':
-            file = open("to_do.txt", 'r', encoding="UTF-8")
-            file.seek(0)
-            print(file.readline())
-    user_input = input("To  exit you can write 'exit', or press 'enter' to countinue")
-    if (user_input == 'exit'):
+    #test
+    print(f'Your task date is {task_date}')
+
+    file = open("to-do list upgrade.txt", "a+", encoding="UTF -8")
+    file.write(str(i))
+    file.write("- "+ task_title + " - "+ str(task_date) + " "+str(task_time) + "\n" )
+    file.seek(0)
+    print(file.readline())
+    file.close()
+    if task_title == "exit":
         break
-print('Thank you for using To-Do program, come back again soon')
+    
