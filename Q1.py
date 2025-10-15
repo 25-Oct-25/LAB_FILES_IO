4f4eb3cf9bb53066bcbf694cf05f331855c60b34
AAQ224 <AAQ_224@hotmail.com>
2025-10-15 11:15:28 +0300

    Abdulrahman Al-Qahtani (add def)

--- Q1.py
+++ Q1.py
@@ -1,25 +1,29 @@
 from datetime import datetime
 now:str = datetime.now()
 
+def add():
+    user_note = input("Type in your new To-Do item: ")
+    to_do = open('to_do.txt', "a+",encoding= "utf-8")
+    to_do.write(user_note + " " + str(now) + "\n")
+    to_do.close()
+
+def read()->str:
+    to_do = open('to_do.txt', "r",encoding= "utf-8")
+    content:str = to_do.read()
+    to_do.close()
+    print(content)
 
 while True:
-    user_input:str = input("do you want to add a new To-Do item? answer by 'y' for yes, 'n' for no and 'exit' for end the program: ")
+    user_input:str = input("do you want to add a new To-Do item? answer by 'y' for yes, 'n' for no and 'exit' for end the program: ").lower()
     
     if user_input == "y":
-        user_note = input("Type in your new To-Do item: ")
-        to_do = open('to_do.txt', "a+",encoding= "utf-8")
-        to_do.write(user_note + " " + str(now) + "\n")
-        to_do.close()
-        user_input = ""
+        add()
 
     elif user_input == "n":
-        user_input = input("do you want to list your To-Do items? answer by 'y' for yes, 'n' for no and 'exit' for end the program: ")
+        user_input = input("do you want to list your To-Do items? answer by 'y' for yes, 'n' for no and 'exit' for end the program: ").lower()
         
         if user_input == "y":
-            to_do = open('to_do.txt', "r",encoding= "utf-8")
-            content:str = to_do.read()
-            to_do.close()
-            print(content)
+            read()
 
         elif user_input == "n":
             pass
